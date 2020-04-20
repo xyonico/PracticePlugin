1f51febe3261265a41273ee9ff5f4ff60e19b51f
Kyle1413 <kylet1412@gmail.com>
2020-04-20 17:41:42 -0400

    formatting

--- PracticePlugin/SongSeekBeatmapHandler.cs
+++ PracticePlugin/SongSeekBeatmapHandler.cs
@@ -5,36 +5,36 @@ using UnityEngine;
 
 namespace PracticePlugin
 {
-	public static class SongSeekBeatmapHandler
-	{	
-		private static List<BeatmapObjectCallbackController.BeatmapObjectCallbackData> CallbackList
-		{
-			get
-			{
-				if (_beatmapObjectCallbackController == null || _callbackList == null)
-				{
-					_beatmapObjectCallbackController = Resources.FindObjectsOfTypeAll<BeatmapObjectCallbackController>()
-						.FirstOrDefault();
+    public static class SongSeekBeatmapHandler
+    {
+        private static List<BeatmapObjectCallbackController.BeatmapObjectCallbackData> CallbackList
+        {
+            get
+            {
+                if (_beatmapObjectCallbackController == null || _callbackList == null)
+                {
+                    _beatmapObjectCallbackController = Resources.FindObjectsOfTypeAll<BeatmapObjectCallbackController>()
+                        .FirstOrDefault();
 
-					if (_beatmapObjectCallbackController != null)
-					{
-						_callbackList =
-							_beatmapObjectCallbackController
-								.GetPrivateField<List<BeatmapObjectCallbackController.BeatmapObjectCallbackData>>(
+                    if (_beatmapObjectCallbackController != null)
+                    {
+                        _callbackList =
+                            _beatmapObjectCallbackController
+                                .GetPrivateField<List<BeatmapObjectCallbackController.BeatmapObjectCallbackData>>(
                                     "_beatmapObjectCallbackData");
-						
-						_beatmapData = _beatmapObjectCallbackController
-							.GetPrivateField<BeatmapData>("_beatmapData");
+
+                        _beatmapData = _beatmapObjectCallbackController
+                            .GetPrivateField<BeatmapData>("_beatmapData");
                     }
 
-					if (_beatmapObjectSpawnController == null)
-					{
-						_beatmapObjectSpawnController = Resources.FindObjectsOfTypeAll<BeatmapObjectSpawnController>()
-							.FirstOrDefault();
+                    if (_beatmapObjectSpawnController == null)
+                    {
+                        _beatmapObjectSpawnController = Resources.FindObjectsOfTypeAll<BeatmapObjectSpawnController>()
+                            .FirstOrDefault();
 
-					}
+                    }
 
-                    if(_beatmapObjectManager == null)
+                    if (_beatmapObjectManager == null)
                     {
                         _beatmapObjectManager = Resources.FindObjectsOfTypeAll<BeatmapObjectManager>().FirstOrDefault();
 
@@ -47,97 +47,97 @@ namespace PracticePlugin
                                 _beatmapObjectManager.GetPrivateField<ObstacleController.Pool>("_obstaclePool");
                         }
                     }
-					if (_noteCutSoundEffectManager == null)
-					{
-						_noteCutSoundEffectManager = Resources.FindObjectsOfTypeAll<NoteCutSoundEffectManager>()
-							.FirstOrDefault();
-					}
-				}
+                    if (_noteCutSoundEffectManager == null)
+                    {
+                        _noteCutSoundEffectManager = Resources.FindObjectsOfTypeAll<NoteCutSoundEffectManager>()
+                            .FirstOrDefault();
+                    }
+                }
 
-				return _callbackList;
-			}
-		}
+                return _callbackList;
+            }
+        }
 
-		private static List<BeatmapObjectCallbackController.BeatmapObjectCallbackData> _callbackList;
-		private static BeatmapObjectCallbackController _beatmapObjectCallbackController;
-		private static BeatmapObjectSpawnController _beatmapObjectSpawnController;
+        private static List<BeatmapObjectCallbackController.BeatmapObjectCallbackData> _callbackList;
+        private static BeatmapObjectCallbackController _beatmapObjectCallbackController;
+        private static BeatmapObjectSpawnController _beatmapObjectSpawnController;
         private static BeatmapObjectManager _beatmapObjectManager;
-		private static NoteCutSoundEffectManager _noteCutSoundEffectManager;
+        private static NoteCutSoundEffectManager _noteCutSoundEffectManager;
 
-		private static NoteController.Pool _noteAPool;
-		private static NoteController.Pool _noteBPool;
-		private static NoteController.Pool _bombNotePool;
-		private static ObstacleController.Pool _obstaclePool;
-		
-		private static BeatmapData _beatmapData;
+        private static NoteController.Pool _noteAPool;
+        private static NoteController.Pool _noteBPool;
+        private static NoteController.Pool _bombNotePool;
+        private static ObstacleController.Pool _obstaclePool;
 
-		public static void OnSongTimeChanged(float newSongTime, float aheadTime)
-		{
-            if(_beatmapObjectCallbackController)
-            _beatmapData = _beatmapObjectCallbackController.GetPrivateField<BeatmapData>("_beatmapData");
-			foreach (var callbackData in CallbackList)
-			{
-				for (var i = 0; i < _beatmapData.beatmapLinesData.Length; i++)
-				{
-					callbackData.nextObjectIndexInLine[i] = 0;
-					while (callbackData.nextObjectIndexInLine[i] < _beatmapData.beatmapLinesData[i].beatmapObjectsData.Length)
-					{
-						var beatmapObjectData = _beatmapData.beatmapLinesData[i].beatmapObjectsData[callbackData.nextObjectIndexInLine[i]];
-						if (beatmapObjectData.time - aheadTime >= newSongTime)
-						{
-							break;
-						}
-						
-						callbackData.nextObjectIndexInLine[i]++;
-					}
-				}
-			}
+        private static BeatmapData _beatmapData;
 
-			var newNextEventIndex = 0;
-			
-			while (newNextEventIndex < _beatmapData.beatmapEventData.Length)
-			{
-				var beatmapEventData = _beatmapData.beatmapEventData[newNextEventIndex];
-				if (beatmapEventData.time >= newSongTime)
-				{
-					break;
-				}
-				
-				newNextEventIndex++;
-			}
-			
-			_beatmapObjectCallbackController.SetPrivateField("_nextEventIndex", newNextEventIndex);
+        public static void OnSongTimeChanged(float newSongTime, float aheadTime)
+        {
+            if (_beatmapObjectCallbackController)
+                _beatmapData = _beatmapObjectCallbackController.GetPrivateField<BeatmapData>("_beatmapData");
+            foreach (var callbackData in CallbackList)
+            {
+                for (var i = 0; i < _beatmapData.beatmapLinesData.Length; i++)
+                {
+                    callbackData.nextObjectIndexInLine[i] = 0;
+                    while (callbackData.nextObjectIndexInLine[i] < _beatmapData.beatmapLinesData[i].beatmapObjectsData.Length)
+                    {
+                        var beatmapObjectData = _beatmapData.beatmapLinesData[i].beatmapObjectsData[callbackData.nextObjectIndexInLine[i]];
+                        if (beatmapObjectData.time - aheadTime >= newSongTime)
+                        {
+                            break;
+                        }
 
-			var notesA = _noteAPool.activeItems.ToList();
-			foreach (var noteA in notesA)
-			{
+                        callbackData.nextObjectIndexInLine[i]++;
+                    }
+                }
+            }
+
+            var newNextEventIndex = 0;
+
+            while (newNextEventIndex < _beatmapData.beatmapEventData.Length)
+            {
+                var beatmapEventData = _beatmapData.beatmapEventData[newNextEventIndex];
+                if (beatmapEventData.time >= newSongTime)
+                {
+                    break;
+                }
+
+                newNextEventIndex++;
+            }
+
+            _beatmapObjectCallbackController.SetPrivateField("_nextEventIndex", newNextEventIndex);
+
+            var notesA = _noteAPool.activeItems.ToList();
+            foreach (var noteA in notesA)
+            {
                 //               Console.WriteLine("Despawning, Length: " + notesA.Count);
                 _beatmapObjectManager.Despawn(noteA);
-			}
-			
-			var notesB = _noteBPool.activeItems.ToList();
-			foreach (var noteB in notesB)
-			{
+            }
+
+            var notesB = _noteBPool.activeItems.ToList();
+            foreach (var noteB in notesB)
+            {
                 _beatmapObjectManager.Despawn(noteB);
-			}
-			
-			var bombs = _bombNotePool.activeItems.ToList();
-			foreach (var bomb in bombs)
-			{
+            }
+
+            var bombs = _bombNotePool.activeItems.ToList();
+            foreach (var bomb in bombs)
+            {
                 _beatmapObjectManager.Despawn(bomb);
-			}
-			
-			var obstacles = _obstaclePool.activeItems.ToList();
-			foreach (var obstacle in obstacles)
-			{
+            }
+
+            var obstacles = _obstaclePool.activeItems.ToList();
+            foreach (var obstacle in obstacles)
+            {
                 _beatmapObjectManager.Despawn(obstacle);
-			}
-			
-			
-			Plugin.AudioTimeSync.SetPrivateField("_prevAudioSamplePos", -1);
+            }
+
+
+            Plugin.AudioTimeSync.SetPrivateField("_prevAudioSamplePos", -1);
             Plugin.AudioTimeSync.SetPrivateField("_songTime", newSongTime);
-			_noteCutSoundEffectManager.SetPrivateField("_prevNoteATime", -1);
-			_noteCutSoundEffectManager.SetPrivateField("_prevNoteBTime", -1);
-		}
-	}
+            _noteCutSoundEffectManager.SetPrivateField("_prevNoteATime", -1);
+            _noteCutSoundEffectManager.SetPrivateField("_prevNoteBTime", -1);
+        }
+    }
 }
